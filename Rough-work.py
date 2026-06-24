fc40d2bb6a88d30cc165dd91871b10f297884a8b
Brenjoel <brenjoelsikha@gmail.com>
2026-06-24 18:18:41 +0530

    Chunking and splitting done

--- Rough-work.py
+++ Rough-work.py
@@ -160,3 +160,91 @@ def item_getter():
 from langchain_ollama import OllamaEmbeddings
 
 print(OllamaEmbeddings.model_fields.keys())
+
+def test_embedding_batch_documents():
+    import os
+    import ssl 
+
+    import asyncio
+    from typing import Any , Dict , List
+
+    import certifi
+    from dotenv import load_dotenv
+
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    # from langchain_chroma import Chroma
+    from langchain_core.documents import Document
+    from langchain_ollama import OllamaEmbeddings
+    from langchain_pinecone import PineconeVectorStore
+    from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+    from logger import (Colors , log_error, log_header ,log_info,log_success,log_warning)
+
+
+    load_dotenv()
+    Ollama_embedding_model = "nomic-embed-text"
+
+
+    # Configure SSL context to use Certifi certifications
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    os.environ["SSL_CERT_FILE"] = certifi.where()
+    os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+    embeddings = OllamaEmbeddings(
+        model = Ollama_embedding_model
+    )
+    # For openAI models you can use the following parameters , chunk_size = 50, retry_min_seconds=10, show_progress_bar= True
+
+    # chroma = Chroma(persist_directly = "chroma_db" , embedding_function= embeddings)
+    vector_store = PineconeVectorStore(index_name=os.environ["INDEX_NAME"], embedding=embeddings)
+    tavily_extract = TavilyExtract()
+    tavily_map = TavilyMap(max_depth = 5, max_breadth = 20 , max_pages = 1000)
+    tavily_crawl = TavilyCrawl()
+
+
+    async def index_documents_async(documents : List[Document], batch_size: int = 50):
+        """ Process documents in batches asyncronously"""
+        
+        # Create batches
+        batches = [
+            documents[i: i+ batch_size] for i in range(0,len(documents), batch_size)
+        ]
+
+        async def add_batch(batch: List[Document], batch_num: int)  :
+            try: 
+                await vector_store.aadd_documents(batch)
+                
+            except Exception as e:
+                return False
+            return True
+        
+        #  Process batches concurrently
+        tasks = [add_batch(batch,i+1) for i,batch in enumerate(batches) ]
+        results = await  asyncio.gather(*tasks , return_exceptions=True)
+
+        successful = sum(1 for result in results if result is True)
+
+
+        print("Done")
+
+    async def main():
+        """Main async function to orchestarates the entire process"""
+
+        res = tavily_crawl.invoke(
+            {
+                "url" : "https://python.langchain.com/",
+                "max_depth":2,
+                "extract_depth": "advanced",
+                "instructions":"ALL" # give any insrtruction to get the results based on the instructions Ex: "content on AI agents"
+            }
+        )
+
+        all_docs = [Document(page_content=result['raw_content'], metadata= {"source":result['url']}) for result in res['results'] ]
+        
+        text_splitters = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splitted_docs = text_splitters.split_documents(all_docs)
+        await index_documents_async(splitted_docs,batch_size=500)
+
+    if __name__ == "__main__":
+        asyncio.run(main())
+
