69eeb06a18c24fe51d741ed73871d525a3e73b6e
Brenjoel <brenjoelsikha@gmail.com>
2026-07-07 15:21:11 +0530

    Uploded all the files from previous commit

---
+++ Agentic_RAG/graph/nodes/generate.py
@@ -0,0 +1,11 @@
+from typing import Any , Dict
+from graph.chains.generation import generation_Chain
+from graph.state import GraphState
+
+def generate(state: GraphState) -> Dict[str,Any]:
+    print("---Generate---")
+    question = state["question"]
+    documents = state["documents"]
+
+    generation = generation_Chain.invoke({"context":documents,"question":question})
+    return {"documents":documents, "question":question,"generation":generation}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from adaptive-agentic-rag!")
+
+
+if __name__ == "__main__":
+    main()
